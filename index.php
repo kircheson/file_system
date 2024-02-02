46064251cf9c9b8689bcfecd14964375db6f5593
kircheson <kirill.kotlyaren@mail.ru>
2024-02-02 13:24:42 +0500

    Рабочий код, задача реализована, но с изменением ТЗ: 1 lvl - 2 знака; 2 lvl - 4 знака; 3 lvl - 6 знаков; и т.д.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-function generateUniqueRandomStrings($length, $quantity) {
+function generateUniqueRandomStrings($length, $quantity)
+{
     $strings = [];
     $attempts = 0;
 
@@ -8,7 +9,7 @@ function generateUniqueRandomStrings($length, $quantity) {
         $bytes = random_bytes($length / 2);
         $string = bin2hex($bytes);
 
-        // Убедимся, что строка уникальна
+        // Убеждаемся, что строка уникальна
         if (!isset($strings[$string])) {
             $strings[$string] = true;
         }
@@ -16,12 +17,26 @@ function generateUniqueRandomStrings($length, $quantity) {
         // На всякий случай, ограничим количество попыток, чтобы избежать бесконечного цикла
         $attempts++;
         if ($attempts > $quantity * 2) {
-            throw new Exception("Too many attempts, unique strings generation is problematic.");
+            throw new Exception("Слишком много попыток, генерация уникальных строк невозможна");
         }
     }
 
     return array_keys($strings);
 }
 
-// Вызываем функцию для генерации 100000 уникальных строк длиной 40 символов
-$uniqueRandomStrings = generateUniqueRandomStrings(40, 100000);
+$root = 'Data';
+$fileNames = generateUniqueRandomStrings(40, 100);
+foreach ($fileNames as $fileName) {
+    $dirPath = 'Data';
+    var_dump($fileName);
+    for ($i = 0; $i < strlen($fileName); $i = $i + 1) {
+        $dirName = substr($fileName, $i, $i++);
+        $dirPath = $dirPath . '/' . $dirName;
+        if (is_dir($dirPath)) {
+            continue;
+        }
+        mkdir($dirPath);
+
+    }
+    file_put_contents($dirPath . '/' . $fileName, $fileName . '.txt');
+}
